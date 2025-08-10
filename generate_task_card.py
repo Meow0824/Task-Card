15dabaf6b2361c8d0ec852f5d8186223a3c68a9f
Meow0824 <luna.gaming0824@gmail.com>
2025-08-10 14:48:43 +0800

    Add files via upload

--- generate_task_card.py
+++ generate_task_card.py
@@ -102,4 +102,7 @@ def draw_card():
     return send_file(img_io, mimetype="image/png")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    import os
+
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
